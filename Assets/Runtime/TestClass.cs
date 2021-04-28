d777905ec213bae77517069b6012e2c7921682b4
liushan <271574862@qq.com>
2021-04-28 18:26:06 +0800

    Package测试工程
    Package测试工程

---
+++ Assets/Runtime/TestClass.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class TestClass
+    {
+        public static void Test()
+        {
+            Debug.Log("Test");
+        }
+    }
+}
